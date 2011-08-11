12ceb284ae5caa8de89ae9369a1f0dc49e4ee248
Roger Chen <roger@rogerhub.com>
2011-08-11 16:07:11 -0700

    signup buttons

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -102,7 +102,7 @@ class SettingsController < ApplicationController
     return render :text => "Wrong old password inputted.#{goback}" if $user.password != params[:cpwold] #no hash here
     # return render :text => "Your new password is too short." if params[:cpwnew].length < 6
     #whoops sorry, can't enforce this on a hashed password. i'll just do it client sided and leave it up to the user.
-    $user.update_attributes(:password => params[:cpwnew]) #no hash here either.. i guess
+    $user.update_attributes(:password => params[:cpwnew],:rememberhash => "") #no hash here either.. i guess
     session[:message] = "Your password has been changed."
     redirect_to "/settings"
   end
