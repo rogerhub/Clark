152e878e217d1dc7807e211eefd08f049a15ed97
Roger Chen <roger@rogerhub.com>
2011-08-10 19:14:28 -0700

    checkbox styles

--- public/javascripts/password.js
+++ public/javascripts/password.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
 		var subhash = hex_sha512($("#login_password").val() + levelone);
 		var superhash = hex_sha512(subhash + auth_challenge);
 		$("#sl_superhash").val(superhash);
-        $("#login_studentid, #login_password").css("background","#A0BCE9 !important"); //metaphors, yeahhh
+        $("#login_studentid, #login_password").css("background","#A0BCE9"); //no important tag for FF5 works
         if ($("#login_remember").val() == "remember"){
         	$("#sl_remember").val("remember");
         }
         $("#login_button").css("visibility","hidden");
         $("#loadingbutton").show();
-		$("#sl_form").submit();		
+		$("#sl_form").submit();
 	});
 });
