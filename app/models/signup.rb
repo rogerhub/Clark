b1c1c1742da6b8b04da43e7ae1098d6cd16f6286
Roger Chen <roger@rogerhub.com>
2011-08-17 20:28:03 -0700

    signup model difficulty did not report value in signup table, but in event table

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -2,6 +2,6 @@ class Signup < ActiveRecord::Base
   belongs_to :account
   belongs_to :event
   def pointdifficulty
-    "#{pointvalue} #{event.difficulty.capitalize()}"
+    "#{pointvalue} #{difficulty.capitalize()}"
   end
 end
