fe36ee39ffb7d5b026da9ed6bf0d22d4981743e6
Roger Chen <roger@rogerhub.com>
2011-09-27 18:34:46 -0700

    added tumblr-google search

--- public/javascripts/goog.js
+++ public/javascripts/goog.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
 	$("#googgo").live('click',function(){
 		location.href="http://google.com/search?q=" + encodeURIComponent($("#googsearch").val()) + "%20site:walnutnhs.com";		
 	});
+	$("#bloggooggo").live('click',function(){
+		location.href="http://google.com/search?q=" + encodeURIComponent($("#bloggoogsearch").val()) + "%20site:blog.walnutnhs.com";		
+	});
 });
