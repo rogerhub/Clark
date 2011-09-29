9a2badc4defce7bc19768ee269a6216d9d60ad64
Roger Chen <roger@rogerhub.com>
2011-09-28 18:36:37 -0700

    critical timezone offset bug

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config.time_zone = 'Pacific Time (US & Canada)'
+
 # Initialize the rails application
 Clark::Application.initialize!
