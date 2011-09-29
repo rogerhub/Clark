0d8f19356a6e3d3bcc917629bc5c89cf81809a16
Roger Chen <roger@rogerhub.com>
2011-09-28 18:38:59 -0700

    regression: timezone

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.time_zone = 'Pacific Time (US & Canada)'
-
 # Initialize the rails application
 Clark::Application.initialize!
