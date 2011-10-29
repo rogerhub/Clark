6a9894f0dab7708f7685a956ca9b8c7f4676ab88
Roger Chen <roger@rogerhub.com>
2011-10-28 19:18:50 -0700

    added related events revision 3

--- app/controllers/volunteer_controller.rb
+++ app/controllers/volunteer_controller.rb
@@ -141,7 +141,7 @@ class VolunteerController < ApplicationController
         
     @postinglist = Posting.find(:all,:conditions => ['event_id = ?',params[:event_id]])
 
-    @relatedevents = Event.find(:all,:conditions => ['lower(name) ILIKE ?',"%#{params[:event_id].gsub(/\(.*\)/,"").lowercase}%"])
+    @relatedevents = Event.find(:all,:conditions => ['lower(name) ILIKE ?',"%#{params[:event_id].gsub(/\(.*\)/,"").downcase}%"])
   end
   def activelisting
     @pagetitle = "Active Event Listings &ndash; WalnutNHS".html_safe
