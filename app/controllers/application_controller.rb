ec27f95a13aa76fca6eecfcb6bf2a53993b547b9
Roger Chen <roger@rogerhub.com>
2011-11-13 16:16:52 -0800

    switched to www2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,8 @@ class ApplicationController < ActionController::Base
   end
 before_filter :check_uri
 	def check_uri
-	  if /^www/.match(request.host)
-		redirect_to request.protocol + request.host_with_port[4..-1] + request.request_uri 
-	  end
-	end
+    redirect_to request.protocol + "www." + request.host_with_port + request.request_uri if !/^www/.match(request.host)
+  end
 end
 def checkmaintenance
   maintain_on = Setting.find_by_name('maintenance').value || "off"
